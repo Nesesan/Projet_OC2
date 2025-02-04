d79cc7dbe85614bef86469a2d7a782d01ae7898c
Nesesan <lounes.benz@outlook.fr>
2025-02-04 11:20:31 +0100

    Add last details for the app

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Olympic } from '../../core/models/Olympic';
-import {Participation} from "../../core/models/Participation";
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
+import { EChartsOption } from 'echarts';
+import { ECElementEvent } from 'echarts';
+import {CallbackDataParams} from "echarts/types/dist/shared";
 
 @Component({
   selector: 'app-home',
@@ -12,40 +14,50 @@ import {Router} from "@angular/router";
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[] | null> = of(null);
+  public isLoading$: Observable<boolean> | undefined;
+  public errorMessage$: Observable<string | null> | undefined;
 
-  pieChartOptions: any;
-  constructor(private olympicService: OlympicService,
-              private router: Router) {}
+  pieChartOptions!: EChartsOption;
+
+  constructor(private olympicService: OlympicService, private router: Router) {}
 
   ngOnInit(): void {
-
+    this.isLoading$ = this.olympicService.getLoadingStatus();
+    this.errorMessage$ = this.olympicService.getErrorMessage();
     this.olympics$ = this.olympicService.getOlympics();
-
 
     this.olympics$.subscribe((olympics) => {
       if (olympics) {
         this.initializePieChart(olympics);
       }
     });
+
+    this.olympicService.loadInitialData().subscribe();
   }
 
   initializePieChart(olympics: Olympic[]): void {
-
-    const medalDistribution = olympics.map((country) => {
-      const totalMedals = country.participations.reduce(
-        (sum, participation) => sum + participation.medalsCount,
-        0
-      );
-      return { name: country.country, value: totalMedals };
-    });
+    const medalDistribution = olympics.map((country) => ({
+      name: country.country,
+      value: country.participations.reduce((sum, p) => sum + p.medalsCount, 0),
+    }));
 
     const colors = ['#876065', '#bccae5', '#8fa0d8', '#6b3f53', '#907f9e'];
-
 
     this.pieChartOptions = {
       tooltip: {
         trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ',
+        backgroundColor: '#4e828e',
+        borderColor: '#4e828e',
+        textStyle: { color: 'white', fontSize: 20 },
+        formatter: function (params: CallbackDataParams | CallbackDataParams[]) {
+          if (Array.isArray(params)) {
+            const param = params[0];
+            return `${param.name} <br/> <img src="assets/medaille.png" alt="medal" style="width: 24px; height: 24px; vertical-align: middle;" /> ${param.value}`;
+          }
+
+          return `
+                  ${params.name} <br/> <img src="assets/medaille.png" alt="medal" style="width: 24px; height: 24px; vertical-align: middle;" />${params.value}`;
+        },
       },
       series: [
         {
@@ -54,10 +66,7 @@ export class HomeComponent implements OnInit {
           radius: '90%',
           data: medalDistribution,
           color: colors,
-          label: {
-            show: true,
-            fontSize:30,
-          },
+          label: { show: true, fontSize: 30 },
           emphasis: {
             itemStyle: {
               shadowBlur: 10,
@@ -68,22 +77,16 @@ export class HomeComponent implements OnInit {
         },
       ],
     };
-
   }
+
   getOlympicGamesCount(olympics: Olympic[]): number {
-    const olympicYears = new Set<number>();
-
-    olympics.forEach((country: Olympic) => {
-      country.participations.forEach((participation: Participation) => {
-        olympicYears.add(participation.year);
-      });
-    });
-
-    return olympicYears.size;
+    return new Set(olympics.flatMap((c) => c.participations.map((p) => p.year))).size;
   }
 
-  onChartClick(event: any): void {
-    const countryName = event.name;
-    this.router.navigate(['/details', countryName]);
+  onChartClick(event: ECElementEvent): void {
+    const countryName = event.name; // ✅ Type correct
+    if (countryName) {
+      this.router.navigate(['/details', countryName]);
+    }
   }
 }
